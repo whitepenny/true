42bec8a6b54308194f61fb8489a2cfe697e0814f
Ben Plum <mr@benplum.com>
2018-11-28 13:37:45 -0500

    Adding investors to placements

--- wp-content/themes/true-search/template-parts/placement-post.php
+++ wp-content/themes/true-search/template-parts/placement-post.php
@@ -1,6 +1,13 @@
 <?php
 $thumb = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
 $term_classes = ts_get_placement_classes( $post->ID );
+
+$investors = get_field( 'placement_investors' );
+$investors_label = get_field( 'placement_investors_label' );
+
+if ( empty( $investors_label ) ) {
+  $investors_label = 'Investors(s)';
+}
 ?>
 <div class="placement-preview <?php echo $term_classes; ?>" style="display: none;">
   <div class="placement-preview__container">
@@ -12,6 +19,14 @@ $term_classes = ts_get_placement_classes( $post->ID );
       <ul>
         <li><?php the_field('placement_location'); ?></li>
       </ul>
+      <?php if ( ! empty( $investors ) ) : ?>
+      <h4 class="placement-investors_label"><?php echo $investors_label; ?></h4>
+      <ul class="placement-investors_list">
+        <?php foreach ( $investors as $investor ) : ?>
+        <li><?php echo $investor['name']; ?></li>
+        <?php endforeach; ?>
+      </ul>
+      <?php endif; ?>
     </div>
   </div>
 </div>
